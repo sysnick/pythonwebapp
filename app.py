1ca45c5cb4a443b0a394a3412e91af01e7a54335
sysnick <nick.hou@outlook.com>
2017-12-15 16:23:03 +0800

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from aiohttp import web
 
 def index(request):
-    return web.Response(body=b'<h1>Awesome</h1>')
+    return web.Response(body='Awesome')
 
 @asyncio.coroutine
 
